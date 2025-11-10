8c3e782b7a05db8a700f789bc0d3b9745ab59d71
Erick Reis <erickcorreareis@gmail.com>
2025-11-10 01:39:58 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
-export default defineConfig({
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    allowedHosts: ['.trycloudflare.com'],
-  },
-  plugins: [
-    tailwindcss(),
-    tsConfigPaths(),
-    tanstackStart(),
-    viteReact(),
-    {
-      ...netlify(),
-      apply: 'build',
+export default defineConfig((ctx) => {
+  return {
+    server: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+      allowedHosts: ['.trycloudflare.com'],
     },
-  ],
+    plugins: [
+      tailwindcss(),
+      tsConfigPaths(),
+      tanstackStart(),
+      viteReact(),
+      ctx.command === 'build' ? netlify() : null,
+    ],
+  }
 })
